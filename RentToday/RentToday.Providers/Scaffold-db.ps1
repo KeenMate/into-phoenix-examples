7a4883ad15b3ac5dfe9c5a8c877e89dbb0d7a283
Filip Jakab <JakabFilip.FJ@gmail.com>
2020-02-01 03:09:39 +0100

    Backend UI creating, EF Database models

---
+++ RentToday/RentToday.Providers/Scaffold-db.ps1
@@ -0,0 +1,1 @@
+dotnet ef dbcontext scaffold "Database=renttoday;Server=db-01.km8.es;Port=5432;User Id=fjakab;Password=Sla kacka do shopinu." Npgsql.EntityFrameworkCore.PostgreSQL -c RentTodayDbContext
